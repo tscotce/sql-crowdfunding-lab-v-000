5fd6148ca291287ddc12b9e838f3e5c6b4c0934f
Tatyana Scotce <tscotce@gmail.com>
2018-06-01 19:51:36 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -4,7 +4,11 @@
   -- funding goal
   -- start date
   -- end date
+<<<<<<< HEAD
 CREATE TABLE projects(id INTEGER PRIMARY KEY, title TEXT, category TEXT, funding_goal INTEGER, start_date TEXT, end_date TEXT);  
+=======
+CREATE TABLE projects(id INTEGER PRIMARY KEY, title TEXT, category TEXT, funding_goal REAL, start_date TEXT, end_date TEXT);  
+>>>>>>> a8c56b48f9133aeeeb809ec4def5f63b714af53f
 
 -- users
   -- name
@@ -15,4 +19,8 @@ CREATE TABLE users(id INTEGER PRIMARY KEY, name TEXT, age INTEGER);
   -- amount
   -- belongs to a user
   -- belongs to a project
+<<<<<<< HEAD
 CREATE TABLE pledges(id INTEGER PRIMARY KEY, amount INTEGER, user_id INTEGER, project_id INTEGER);  
+=======
+CREATE TABLE pledges(id INTEGER PRIMARY KEY, amount REAL, user_id INTEGER, project_id INTEGER);  
+>>>>>>> a8c56b48f9133aeeeb809ec4def5f63b714af53f
--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -38,6 +38,10 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
   GROUP BY projects.title
   HAVING (SUM(amount) - projects.funding_goal) >= 0 
   ORDER BY projects.title;"
+
+  ORDER BY projects.title
+  HAVING SUM(amount) > 0;"
+
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
